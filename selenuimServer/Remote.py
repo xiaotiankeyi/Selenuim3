51e6cb3c0d32697bddd7ae295dd96c5e0c1cce2b
xiaotiankeyi <laizhitian163@163.com>
2019-11-07 10:49:21 +0800

    update

--- selenuimServer/Remote.py
+++ selenuimServer/Remote.py
@@ -3,24 +3,21 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from time import sleep
-import os
+import os, multiprocessing
 from selenium.webdriver import Remote
 
 '''
 grid启动不同的浏览器来执行测试用例
 '''
-lists = {'http://127.0.0.1:4444/wd/hub': 'chrome',
-         'http://192.168.117.9:5557/wd/hub': 'firefox',
-     }
 
-for ip, browser in lists.items():
+
+def login_succeed(ip, browser):
     driver = Remote(command_executor=ip,
                     desired_capabilities={'platform': 'ANY',
                                           'browserName': browser,
                                           'version': '',
                                           'javascriptEnabled': True,
                                           })
-
     url = "http://192.168.117.9:8080/jforum/forums/list.page"
     driver.get(url)
 
@@ -32,20 +29,32 @@ for ip, browser in lists.items():
         request = driver.find_element(By.ID, 'myprofile').text
         print(request)
 
-        # 登录失败
-        # response = driver.find_element(By.ID,"invalidlogin").text
-        # print(response)
-
         find_digit = driver.find_element_by_xpath("//table[@cellpadding='2']/tbody/tr[4]/td[4]").text
         print(find_digit)
 
         """截图"""
-        # driver.get_screenshot_as_file(r"C:/Users/admin/PycharmProjects/selenuim3/browser/error.png")
-        # driver.get_screenshot_as_file(os.getcwd() + "error.png")
+        driver.save_screenshot(filename='loginSucceed.png')
 
     except NoSuchElementException as e:
         print(e)
 
     finally:
-        sleep(5)
+        sleep(2)
         driver.quit()
+
+
+if __name__ == "__main__":
+
+    lists = {'http://127.0.0.1:5556/wd/hub': 'chrome',
+             'http://127.0.0.1:5557/wd/hub': 'firefox', }
+
+    all = []
+    for ip, browser in lists.items():
+        t = multiprocessing.Process(target=login_succeed, args=(ip, browser))
+        all.append(t)
+
+    for i in range(len(lists)):
+        all[i].start()
+
+    for i in range(len(lists)):
+        all[i].join()
