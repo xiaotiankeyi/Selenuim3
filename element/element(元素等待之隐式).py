ebb8f01b49c53a424fe2f7053bb5be99ae0ae7a6
xiaotiankeyi <laizhitian163@163.com>
2021-04-22 14:04:33 +0800

    更新

--- element/element(元素等待之隐式).py
+++ element/element(元素等待之隐式).py
@@ -1,12 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep,ctime
-
-"""引入鼠标事件"""
-from selenium.webdriver.common.action_chains import ActionChains
-"""引入隐式等待"""
+from selenium.webdriver.common.action_chains import ActionChains    #引入鼠标操作
 from selenium.common.exceptions import NoSuchElementException
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ES
 
 driver = webdriver.Firefox()
 
@@ -16,6 +14,10 @@ url = "http://192.168.117.9/bbs/upload/forum.php"
 driver.get(url)
 
 driver.find_element(By.ID, 'ls_username').send_keys('admin')
+
+"""设置显示等待"""
+element = WebDriverWait(driver, 10, 0.5).until(ES.presence_of_element_located((By.ID, 'KW')))   #自定义
+ele = WebDriverWait(driver, 10).until(lambda x: x.find_element_by_id("someId"))     #官方提供
 
 addr = driver.find_element(By.NAME, 'password')
 addr.clear()
